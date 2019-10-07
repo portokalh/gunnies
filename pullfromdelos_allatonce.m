50aca433c77b334b2ef7aad7984a7f9b726079de
AlexBadea <portokalh@gmail.com>
2019-10-07 18:47:27 -0400

    Add files via upload

--- pullfromdelos_allatonce.m
+++ pullfromdelos_allatonce.m
@@ -12,8 +12,7 @@ fprintf(sfid,'%s\n' ,shebang)
 %sfid=fopen(mysbatch_file, 'w');
 %fprintf(sfid,'%s\n' ,shebang)
 
-runno={'N57446' , 'N57498', 'N57500', 'N57502', 'N57504'};
-
+runno={'N57446' , 'N57498', 'N57500', 'N57502', 'N57504', 'N57433' , 'N57434', 'N57435', 'N57436', 'N57437', 'N57438', 'N57439', 'N57440', 'N57442', 'N57443'};
 for ii=1:numel(runno)
     myname= char(runno(ii))
 
@@ -21,7 +20,7 @@ mysbatch_file=[clus_batch_directory char(runno(ii)) '_sync.bash'];
 fid=fopen(mysbatch_file, 'w');
 fprintf(fid,'%s\n' ,shebang)
    
-cmd1=['rsync -rpv /Volumes/delosspace-1/diffusionN57446dsi_studio-results/nii4D_' char(runno(ii)) '.nii  /Volumes/dusom_abadea_nas1/19abb14'];
+cmd1=['rsync -rpv /Volumes/delosspace-1/diffusion' char(runno(ii)) 'dsi_studio-results/nii4D_' char(runno(ii)) '.nii  /Volumes/dusom_abadea_nas1/19abb14'];
 cmd2=['rsync -rpv /Volumes/delosspace-1/' char(runno(ii)) '*  /Volumes/dusom_abadea_nas1/19abb14'];
 cmd3=['rsync -rpv /Volumes/delosspace-1/' char(runno(ii)) '.work /Volumes/dusom_abadea_nas1/19abb14'];
 cmd4=['rsync -rpv /Volumes/dusom_abadea_nas1/19abb14/Volumes/delosspace-1/co_reg_' char(runno(ii)) '_m00-work /Volumes/dusom_abadea_nas1/19abb14'];
